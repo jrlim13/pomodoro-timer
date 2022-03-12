9aeb53b1efc4cf250cbe0a53737ef6579c394ae9
Jasmin Lim <jrlim113@gmail.com>
2022-03-12 20:34:50 +1100

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header>
+            <h1 className="m-8 text-6xl font-bold">Pomodoro Timer</h1>
+        </header>
+    );
+}
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "red": "#FF5C58",
+        "peach": {
+          100: "#FCD2D1",
+          200: "#FE8F8F"
+        },
+        "beige": "#FFEDD3"
+      }
+    },
+  },
+  plugins: [],
+  darkMode: "class",
+}
